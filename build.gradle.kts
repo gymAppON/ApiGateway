caa25ac2eae84587911689aec7380506ff770474
Belimbb <ol3ksii.bielov@gmail.com>
2025-11-15 23:42:19 +0100

    Built fundamental infrastructure Connected with UserManager microservice

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,9 @@ extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
